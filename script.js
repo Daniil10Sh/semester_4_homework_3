644ebe7c36fe760c4352381c42368ff3da1f6044
Daniil10Sh <danik2022odessa@gmail.com>
2024-11-19 15:03:27 +0200

    add changes

--- script.js
+++ script.js
@@ -1,22 +1,27 @@
+document.addEventListener('DOMContentLoaded', function () {
+  const images = document.querySelectorAll('img[data-src]');
+  const imgOrg = [];
+  const observer = new IntersectionObserver(
+    (entries, observer) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          const img = entry.target;
+          img.src = img.getAttribute('src');
+          imgOrg.push(img);
+          img.src = img.getAttribute('data-src');
 
-import { IntersectionObserver } from 'intersection-observer-polyfill';
-
-const observer = new IntersectionObserver((entries, observer) => {
-  entries.forEach(entry => {
-    if (entry.isIntersecting) {
-      const img = entry.target;
-      img.src = img.getAttribute('data-src');
-      img.classList.remove('loading');
-      observer.unobserve(img);
+          observer.unobserve(img);
+        }
+      });
+    },
+    {
+      root: null,
+      rootMargin: '0px',
+      threshold: 0.1,
     }
-  });
-}, {
-  root: null,
-  rootMargin: '0px',
-  threshold: 0.1 
-});
+  );
 
-const images = document.querySelectorAll('img[data-src]');
-images.forEach(image => {
-  observer.observe(image);
+  images.forEach((image) => {
+    observer.observe(image);
+  });
 });
